75405b3b22f1afea7d0d173c186ff63dd8145f3c
Sebastián Dinator <sdinator@alumnos.uai.cl>
2024-10-03 00:21:39 -0300

    Añade nuevo diseño y efectos al sitio personal: incluye CSS externo, fondo animado de partículas y actualiza la información de contacto.

---
+++ particles.js
@@ -0,0 +1,46 @@
+particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 100,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#08fdd8"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false
+      },
+      "size": {
+        "value": 3,
+        "random": true
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#08fdd8",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 4,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false
+      }
+    }
+  });
+  
