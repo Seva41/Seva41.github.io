bcf26729e73487c1c28f39e943d2aaee98ec393a
Seva41 <Seva41@users.noreply.github.com>
2024-10-24 20:19:59 +0000

    deploy: 4cc67afd4b449103849adb6fe089af7262de1ad1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,38 @@
 // assets/js/main.js
 
 function cambiarIdioma(idioma) {
-    var elementosES = document.querySelectorAll('.es');
-    var elementosEN = document.querySelectorAll('.en');
+  var elementosES = document.querySelectorAll(".es");
+  var elementosEN = document.querySelectorAll(".en");
 
-    if (idioma === 'es') {
-        elementosES.forEach(function(el) { el.style.display = 'block'; });
-        elementosEN.forEach(function(el) { el.style.display = 'none'; });
-    } else {
-        elementosES.forEach(function(el) { el.style.display = 'none'; });
-        elementosEN.forEach(function(el) { el.style.display = 'block'; });
-    }
+  if (idioma === "es") {
+    elementosES.forEach(function (el) {
+      el.style.display = "block";
+    });
+    elementosEN.forEach(function (el) {
+      el.style.display = "none";
+    });
+  } else {
+    elementosES.forEach(function (el) {
+      el.style.display = "none";
+    });
+    elementosEN.forEach(function (el) {
+      el.style.display = "block";
+    });
+  }
 
-    // Guardar la selección de idioma en LocalStorage
-    localStorage.setItem('idiomaSeleccionado', idioma);
+  // Guardar la selección de idioma en LocalStorage
+  localStorage.setItem("idiomaSeleccionado", idioma);
 }
 
 // Función para alternar el menú en pantallas móviles
 function toggleMenu() {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('nav-links-active');
+  const navLinks = document.querySelector(".nav-links");
+  navLinks.classList.toggle("nav-links-active");
 }
 
-window.onload = function() {
-    // Verificar si el idioma ya fue seleccionado anteriormente
-    var idiomaGuardado = localStorage.getItem('idiomaSeleccionado') || 'es';
-    cambiarIdioma(idiomaGuardado);
-    document.body.classList.add('loaded');
+window.onload = function () {
+  // Verificar si el idioma ya fue seleccionado anteriormente
+  var idiomaGuardado = localStorage.getItem("idiomaSeleccionado") || "es";
+  cambiarIdioma(idiomaGuardado);
+  document.body.classList.add("loaded");
 };
